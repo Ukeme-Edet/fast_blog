f2a8d3b6b913d256fe082ffdeeb904bd0df5a9eb
Ukeme Edet <ukemeedet2207@gmail.com>
2024-09-25 22:00:41 +0100

    Refactor blog module and add error handling

--- src/data/blog.py
+++ src/data/blog.py
@@ -1,6 +1,9 @@
 import uuid
 from sqlmodel import Field, SQLModel, Session, select, Relationship
-from datetime import datetime
+from datetime import UTC, datetime
+from model.blog import Blog, BlogCreate, BlogUpdate, BlogInDB
+from model.user import User
+from errors.errors import Missing, Duplicate
 from . import engine
 
 
@@ -9,17 +12,19 @@ class Blog(SQLModel, table=True):
         primary_key=True, default_factory=lambda: str(uuid.uuid4())
     )
     user_id: str = Field(foreign_key="user.id")
-    title: str = Field()
-    content: str = Field()
+    title: str = Field(min_length=2, max_length=100, index=True)
+    content: str = Field(min_length=2)
     time_created: datetime = Field(default=datetime.now())
     time_updated: datetime = Field(default=datetime.now())
     user: "User" = Relationship(back_populates="blogs")
 
 
-def get_blog_by_id(id: str):
+def get_blog_by_id(id: str) -> BlogInDB:
     with Session(engine) as session:
         blog = session.exec(select(Blog).where(Blog.id == id)).first()
-        return blog
+        if blog:
+            return blog
+        raise Missing(msg=f"Blog with id {id!r} not found")
 
 
 def get_blogs_by_user_id(user_id: str):
@@ -34,24 +39,53 @@ def get_all_blogs():
         return blogs
 
 
-def create_blog(blog: Blog):
+def create_blog(blog: BlogCreate) -> BlogInDB:
     with Session(engine) as session:
-        session.add(blog)
-        session.commit()
-        session.refresh(blog)
-        return blog
+        try:
+            new_blog = Blog(
+                id=str(uuid.uuid4()),
+                time_created=datetime.now(UTC),
+                time_updated=datetime.now(UTC),
+                **blog.model_dump(),
+            )
+            session.add(new_blog)
+            session.commit()
+            session.refresh(new_blog)
+            return new_blog
+        except Exception as e:
+            raise e
 
 
-def update_blog(blog: Blog):
+def update_blog(blog: BlogUpdate) -> BlogInDB:
     with Session(engine) as session:
-        session.add(blog)
-        session.commit()
-        session.refresh(blog)
-        return blog
+        try:
+            try:
+                blog_db = get_blog_by_id(blog.id)
+            except Missing as e:
+                raise Missing(msg=f"Blog with id {blog.id!r} not found")
+            if blog.title:
+                blog_db.title = blog.title
+            if blog.content:
+                blog_db.content = blog.content
+            blog_db.time_updated = datetime.now(UTC)
+            session.add(blog_db)
+            session.commit()
+            session.refresh(blog_db)
+            return blog_db
+        except Exception as e:
+            session.rollback()
+            raise e
 
 
-def delete_blog(blog: Blog):
+def delete_blog(blog: BlogInDB):
     with Session(engine) as session:
-        session.delete(blog)
-        session.commit()
-        return blog
+        try:
+            try:
+                blog_db = get_blog_by_id(blog.id)
+            except Missing as e:
+                raise e
+            session.delete(blog_db)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            raise e
--- src/model/blog.py
+++ src/model/blog.py
@@ -1,25 +1,46 @@
 from . import BaseModel, datetime
+from pydantic import Field
+from typing import Optional
 
 
 class Blog(BaseModel):
-    id: str
-    user_id: str
-    title: str
-    content: str
-    time_created: datetime
-    time_updated: datetime
+    id: str = Field(..., description="The unique identifier for the blog")
+    user_id: str = Field(..., description="The unique identifier for the user")
+    title: str = Field(
+        ..., max_length=100, description="The title of the blog", min_length=2
+    )
+    content: str = Field(
+        ..., description="The content of the blog", min_length=2
+    )
+    time_created: datetime = Field(
+        ..., description="The time the blog was created"
+    )
+    time_updated: datetime = Field(
+        ..., description="The time the blog was last updated"
+    )
 
 
 class BlogCreate(BaseModel):
-    user_id: str
-    title: str
-    content: str
+    user_id: str = Field(..., description="The unique identifier for the user")
+    title: str = Field(
+        ..., max_length=100, description="The title of the blog", min_length=2
+    )
+    content: str = Field(
+        ..., description="The content of the blog", min_length=2
+    )
 
 
 class BlogUpdate(BaseModel):
-    title: str
-    content: str
-    time_updated: datetime
+    id: str = Field(..., description="The unique identifier for the blog")
+    title: Optional[str] = Field(
+        None, max_length=100, description="The title of the blog", min_length=2
+    )
+    content: Optional[str] = Field(
+        None, description="The content of the blog", min_length=2
+    )
+    time_updated: datetime = Field(
+        ..., description="The time the blog was last updated"
+    )
 
 
 class BlogInDB(Blog):
