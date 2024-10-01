76372b2604c581df9c8cd4a08a83148ef8aa0708
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-01 10:44:02 +0100

    Refactor user_role.py model and add docstring

--- src/model/user_role.py
+++ src/model/user_role.py
@@ -6,5 +6,15 @@ from . import BaseModel
 
 
 class UserRole(BaseModel):
+    """
+    User role model
+
+    This class contains the attributes of the user role model.
+
+    Attributes:
+        id (str): The unique identifier for the user role
+        name (str): The name of the user role
+    """
+
     id: str
     name: str
